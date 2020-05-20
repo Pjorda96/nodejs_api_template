a4a66dc9d898e449a0d308e5963fb5afdaa6f648
pabloPc <pjorda96@gmail.com>
2020-05-20 18:27:15 +0200

    swagger startup and error

--- src/app.js
+++ src/app.js
@@ -2,14 +2,21 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const swaggerUi = require('swagger-ui-express')
 
+const swaggerDoc = require('./swagger')
 const api = require('./routes')
 
 const app = express()
 
+const swaggerOptions = {
+  explorer: true
+};
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDoc, swaggerOptions))
 app.use('/api', api)
 
 module.exports = app
--- src/routes/index.js
+++ src/routes/index.js
@@ -12,7 +12,28 @@ const auth = require('../middleware/authMiddleware')
 const api = express.Router()
 
 
-// user routes
+/**
+ * @swagger
+ * /user:
+ *   get:
+ *     tags:
+ *       — User
+ *     summary: This should create a new ice cream.
+ *     description: This is where you can give some background as to why this route is being created or perhaps reference a ticket number.
+ *     consumes:
+ *       — application/json
+ *     parameters:
+ *       — name: body
+ *       in: body
+ *       schema:
+ *         type: object
+ *         properties:
+ *           flavor:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Receive back flavor and flavor Id.
+ */
 api.get('/user', auth, UserController.getUsers) // comprobar rol
 api.get('/user/:id', auth, UserController.getUser) // comprobar el usuario y el rol
 // no user post, use signup
