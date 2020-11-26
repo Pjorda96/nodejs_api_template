68c2e208fa309fede3c4f75a0313b6a9b48076c3
pabloPc <pjorda96@gmail.com>
2020-11-26 23:35:09 +0100

    absolute health check

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json())
 
 app.use('/api', api)
 
-api.get('/health/check', (req, res) => {
+app.get('/health/check', (req, res) => {
   res.status(200).send({ message: 'Healthy' })
 })
 
