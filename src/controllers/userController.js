7bc9eea1db9c75f4160db2b0a9498faf7bb7c5f7
pabloPc <pjorda96@gmail.com>
2020-11-28 11:42:22 +0100

    user fixes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -8,35 +8,35 @@ function getUsers(req, res) {
     if (err) return res.status(500).send({ message: err })
     if (!users) return res.status(404).send({ message: 'Not users found' })
 
-    res.status(200).send({ users })
+    res.status(200).send(users)
   })
 }
 
 function getUser(req, res) {
-  let id = req.params.id
+  const id = req.params.id
 
   UserModel.findById(id, (err, user) => {
     if (err) return res.status(500).send({ message: err })
     if (!user) return res.status(404).send({ message: 'User not found' })
 
-    res.status(200).send({ user })
+    res.status(200).send(user)
   })
 }
 
 function updateUser(req, res) {
-  let id = req.params.id;
-  let body = req.body;
+  const id = req.params.id;
+  const body = req.body;
 
   UserModel.findByIdAndUpdate(id, body, (err, user) => {
     if (err) res.status(500).send({ message: err });
     if (!user) return res.status(404).send({ message: 'User not found' });
 
-    res.status(200).send({ user });
+    res.status(200).send(user);
   });
 }
 
 function deleteUser(req, res) {
-  let id = req.params.id;
+  const id = req.params.id;
 
   UserModel.findByIdAndDelete(id, null, (err, user) => {
     if (err) res.status(500).send({ message: err });
@@ -47,27 +47,34 @@ function deleteUser(req, res) {
 }
 
 function signUp(req, res) {
-  const user = new UserModel({
-    email: req.body.email,
-    displayName: req.body.displayName,
-    password: req.body.password
-  })
+  const user = new UserModel(req.body)
 
-  user.save(err => {
+  user.save((err, userDb) => {
     if (err) return res.status(500).send({ message: `User creation error: ${err}` })
 
-    return res.status(201).send({ token: AuthService.createToken(user) })
+    const { _id, email, displayName } = userDb
+    return res.status(201).send({
+      id: _id,
+      email,
+      displayName,
+      token: authService.createToken(user)
+    })
   })
 }
 
 function signIn(req, res) {
   UserModel.find({ email: req.body.email }, (err, user) => {
     if (err) return res.status(500).send({ message: err })
-    if (!user) return res.send(404).send({ message: 'User not found' })
+    if (!user || !user.length) return res.status(404).send({ message: 'User not found' })
 
-    req.user = user;
+    req.user = user
+    const { _id, email, displayName } = user[0]
+
     res.status(200).send({
       message: 'Logged in',
+      id: _id,
+      email,
+      displayName,
       token: authService.createToken(user)
     })
   })
