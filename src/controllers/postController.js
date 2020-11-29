badda40a43a8c6ef01c1473e9ed3b8d222cf86ca
pabloPc <pjorda96@gmail.com>
2020-11-29 11:59:18 +0100

    post fixes

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -7,52 +7,45 @@ function getPostAll(req, res) {
     if (err) return res.status(500).send({ message: err })
     if (!post) return res.status(404).send({ message: 'Not post found' })
 
-    res.status(200).send({ post })
+    res.status(200).send(post)
   })
 }
 
 function getPost(req, res) {
-  let id = req.params.id
+  const id = req.params.id
 
   PostModel.findById(id, (err, post) => {
     if (err) return res.status(500).send({ message: err })
     if (!post) return res.status(404).send({ message: 'Post not found' })
 
-    res.status(200).send({ post })
+    res.status(200).send(post)
   })
 }
 
 function createPost(req, res) {
-  console.log('POST /api/post')
-  console.log(req.body)
-
-  let post = new PostModel()
-  post.url = req.body.url
-  post.callNumber = req.body.callNumber
-  post.cathegory = req.body.cathegory
-  post.body = req.body.body
+  const post = new PostModel(req.body)
 
   post.save((err, postRes) => {
     if (err) res.status(500).send({ message: err })
 
-    res.status(200).send({ post: postRes })
+    res.status(200).send(postRes)
   })
 }
 
 function updatePost(req, res) {
-  let id = req.params.id;
-  let body = req.body;
+  const id = req.params.id;
+  const body = req.body;
 
   PostModel.findByIdAndUpdate(id, body, (err, post) => {
     if (err) res.status(500).send({ message: err });
     if (!post) return res.status(404).send({ message: 'Post not found' });
 
-    res.status(200).send({ post });
+    res.status(200).send(post);
   });
 }
 
 function deletePost(req, res) {
-  let id = req.params.id;
+  const id = req.params.id;
 
   PostModel.findByIdAndDelete(id, null, (err, post) => {
     if (err) res.status(500).send({ message: err });
